0f447aee6aa450762f65d033d92dab76e36bd0e0
Denis Shevchenko <me@dshevchenko.biz>
2017-03-25 15:43:40 +0400

    Почему Бананы и Линзы?

--- src/Markup/About.hs
+++ src/Markup/About.hs
@@ -17,16 +17,25 @@ raw = do
     h1 "$title$"
     div ! A.class_ "about-us" $ do
         toHtml weAre
+        why
         contactUs
   where
     weAre :: Text
     weAre = "«Бананы и Линзы» — единственный русскоязычный подкаст, всецело посвящённый языку программирования Haskell. Мы используем этот прекрасный язык в своей повседневной работе и способствуем его распространению в русскоговорящей среде. Последние новости из мира Haskell, передовые разработки и интересные события — всё это в выпусках «Бананов и Линз»."
 
+    why :: Html
+    why =
+        div $ do
+            preEscapedToHtml ("<br/>" :: String)
+            preEscapedToHtml ("Вероятно, вас удивило столь необычное название нашего подкаста. Ноги растут из любопытной публикации " :: String)
+            a ! A.href "http://doc.utwente.nl/56289/1/meijer91functional.pdf" $ "«Functional Programming with Bananas, Lenses, Envelopes and Barbed Wire»"
+            span ". И да, линзы Кметта тут ни при чём…"
+
     contactUs :: Html
     contactUs =
         div $ do
             preEscapedToHtml ("<br/>" :: String)
-            span "Если у вас есть вопросы, пожелания или предложения — напишите"
+            span "Если у вас есть вопросы, пожелания или предложения — пишите"
             a ! A.href "mailto:me@dshevchenko.biz" $ "Денису Шевченко"
             span " или "
             a ! A.href "mailto:alexander.vershilov@gmail.com" $ "Александру Вершилову"
